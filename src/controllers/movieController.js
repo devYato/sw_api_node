8a9f0570e7365fd99768b8cf07997169a8aedbe1
Yato <renannjuina1907@outlook.com>
2024-08-18 15:39:43 -0400

    commit with DELETE and PUT methods

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -16,3 +16,19 @@ exports.getMovies = async (req, res) => {
     const movies = await Movie.find()
     res.send(movies)
 };
+
+exports.deleteMovies = async (req, res) => {
+    const movie = await Movie.findByIdAndRemove(req.params.id)
+    return res.send(movie)
+}
+
+exports.updateMovies = async(req, res) => {
+    const movie = await Movie.findByIdAndUpdate(req.params.id, {
+        title : req.body.title,
+        description : req.body.description,
+        img_url : req.body.img_url,
+        trailer_url : req.body.trailer_url
+    })
+
+    return res.send(movie)
+}
--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -4,5 +4,6 @@ const movieController = require('../controllers/movieController');
 
 router.get('/', movieController.getMovies);
 router.post('/', movieController.createMovie);
-
+router.delete('/:id/', movieController.deleteMovies);
+router.put('/', movieController.updateMovies);
 module.exports = router;
